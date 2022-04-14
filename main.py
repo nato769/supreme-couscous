080ede8c6f19572205f58318cf4d158062949d05
nato769 <101778356+nato769@users.noreply.github.com>
2022-04-14 23:35:32 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -6,72 +6,278 @@ frame = Frame(root)
 label = Label(frame, text="All of the food and drinks have sold out due to popular demand. Please come back tomorrow. \n We are sorry for any inconvenience that this may have caused.")
 frame.pack()
 label.pack()
-items = {
-    "food 0": "Blue Cod Fillets",
-    "food 1": "Chicken Breast",
-    "food 2": "Mixed Grill",
-    "food 3": "New Zealand Ham Steak",
-    "food 4": "Home -made Pie",
-    "food 5": "Crumbed Schnitzel",
-    "food 6": "Lamb Shank(s)",
-    "food 7": "Rib Eye Steak",
-    "food 8": "Pork Belly",
-    "drink a": "Soft Drinks",
-    "drink b": "Mac's Range",
-    "drink c": "Red Bull",
-    "drink d": "Fruit Juices",
-    "drink e": "Sparkling Juice",
-    "drink f": "Tropicana",
-    "drink g": "Red Peach",
-    "drink h": "Citrus Burst",
-    "drink i": "Mango Madness",
-}
-descript = {
-    "descript 0": "Homemade Beer Batter OR Pan-fried W lemon butter. Served W French fries A salad & Tartare sauce.",
-    "descript 1": "Oven baked Chicken W melted brie, cranberries & wrapped in streaky bacon. Served with pea puree & plum sauce. With Vegetables OR French fries & Salad",
-    "descript 2": "Bacon,Sausages, Eggs, Onions, Peppered Tomato,Schnitzel, Mushrooms & French fries",
-    "descript 3": "Prime Cut Ham served W pineapple salsa, grilled tomato, fried egg . Served w French fries & salad.",
-    "descript 4": "Our delicious homemade Pie. Served W creamy mash, mushy peas & Kumara chips. Check Blackboard for today's special",
-    "descript 5": "Served with Red Wine Gravy & Vegetables.",
-    "descript 6": "Slow cooked with onion, tomato, bay leaves, red wine & rosemary, placed on creamy mash.",
-    "descript 7": "Served W French Fries & Salad. Grilled to your liking",
-    "descript 8": "Five Spice Pork belly slow cooked with malt vinegar' & bourbon glaze Served on pumpkin mash W crispy crackling & kumara chips and Vegetables",
-    "descript a": "See Options",
-    "descript b": "See Options",
-    "descript c": "See Options",
-    "descript d": "See Options",
-    "descript e": "See Options",
-    "descript f": "Orange cranberry and pineapple juice with a delicious layer of passionfruit puree",
-    "descript g": "Peach nectar lemonade grenadine and fresh lemon juice",
-    "descript h": "Mango nectar orange juice and apple juice",
-    "descript i": "Mango nectar cranberry juice and fresh lime",
-}
-opts = {
-    "opt a": "Coke/L&P/Sprite/Ginger Beer/Tonic Water/ Lime and soda/Lemon Lime and Bitters",
-    "opt b": "Ginger Beer/ Feijoa and Pear/ Mandarin/ Lime and Bitters/ Lemon and Rhubarb",
-    "opt c": "Regular or sugar free",
-    "opt d": "Orange/Apple/Cranberry/Pineapple/Tomato/Mango/Peach",
-    "opt e": "Blackcurrent/Passionfruit",
-}
-cost = {
-    "cost 0": "M $26.50 / L $33.00",
-    "cost 1": "$29.50",
-    "cost 2": "$26.50",
-    "cost 3": "$25.50",
-    "cost 4": "$22.50",
-    "cost 5": "$22.50",
-    "cost 6": "S $22.50 / D $32.50",
-    "cost 7": "$29.00",
-    "cost 8": "$30.00",
-    "cost a": "$6",
-    "cost b": "$7",
-    "cost c": "$7.50",
-    "cost d": "$6.50",
-    "cost e": "$8.00",
-    "cost f": "$8.50",
-    "cost g": "$8.50",
-    "cost h": "$8.50",
-    "cost i": "$8.50",
-}
+item = [
+    "Blue Cod Fillets",
+    "Chicken Breast",
+    "Mixed Grill",
+    "New Zealand Ham Steak",
+    "Homemade Pie",
+    "Crumbed Schnitzel",
+    "Lamb Shank(s)",
+    "Rib Eye Steak",
+    "Pork Belly",
+    "Soft Drinks",
+    "Mac's Range",
+    "Red Bull",
+    "Fruit Juices",
+    "Sparkling Juice",
+    "Tropicana",
+    "Red Peach",
+    "Citrus Burst",
+    "Mango Madness",
+]
+descript = [
+    "Homemade Beer Batter OR Pan-fried W lemon butter. Served W French fries A salad & Tartare sauce.",
+    "Oven baked Chicken W melted brie, cranberries & wrapped in streaky bacon. Served with pea puree & plum sauce. With Vegetables OR French fries & Salad",
+    "Bacon,Sausages, Eggs, Onions, Peppered Tomato, Schnitzel, Mushrooms & French fries",
+    "Prime Cut Ham served W pineapple salsa, grilled tomato, fried egg . Served w French fries & salad.",
+    "Our delicious homemade Pie. Served W creamy mash, mushy peas & Kumara chips. Check Blackboard for today's special",
+    "Served with Red Wine Gravy & Vegetables.",
+    "Slow cooked with onion, tomato, bay leaves, red wine & rosemary, placed on creamy mash.",
+    "Served W French Fries & Salad. Grilled to your liking",
+    "Five Spice Pork belly slow cooked with malt vinegar' & bourbon glaze Served on pumpkin mash W crispy crackling & kumara chips and Vegetables",
+    "See Options",
+    "See Options",
+    "See Options",
+    "See Options",
+    "See Options",
+    "Orange cranberry and pineapple juice with a delicious layer of passionfruit puree",
+    "Peach nectar lemonade grenadine and fresh lemon juice",
+    "Mango nectar orange juice and apple juice",
+    "Mango nectar cranberry juice and fresh lime",
+]
+opt = [
+    ["Medium", "Large"],
+    "Only one option",
+    "Only one option",
+    "Only one option",
+    "Only one option",
+    "Only one option",
+    ["Single", "Double"],
+    "Only one option",
+    "Only one option",
+    ["Coke" ,"L&P", "Sprite","Ginger Beer", "Tonic Water", "Lime and soda", "Lemon Lime and Bitters"],
+    ["Ginger Beer", "Feijoa and Pear", "Mandarin", "Lime and Bitters", "Lemon and Rhubarb"],
+    ["Regular", "sugar free"],
+    ["Orange", "Apple", "Cranberry", "Pineapple", "Tomato", "Mango", "Peach"],
+    ["Blackcurrent", "Passionfruit"],
+    "Only one option",
+    "Only one option",
+    "Only one option"
+]
+cost = [
+    ["26.50", "33.00"],
+    "29.50",
+    "26.50",
+    "25.50",
+    "22.50",
+    "22.50",
+    ["22.50", "32.50"],
+    "29.00",
+    "30.00",
+    "6",
+    "7",
+    "7.50",
+    "6.50",
+    "8.00",
+    "8.50",
+    "8.50",
+    "8.50",
+    "8.50",
+]
+optyn = [
+    "Y",
+    "N",
+    "N",
+    "N",
+    "N",
+    "N",
+    "Y",
+    "N",
+    "N",
+    "Y",
+    "Y",
+    "Y",
+    "Y",
+    "Y",
+    "N",
+    "N",
+    "N",
+]
+receipt = []
+cost_stuff = []
+hjk=0
 
-root.mainloop()
+def ui(data_type,data):
+    while True:
+        try:
+            if data_type=='int':
+                i = int(input(data))
+            elif data_type=='str':
+                i = input(data)
+            break
+        except:
+            print("please input an iteger from 1 to 17")
+    return i
+for i in range(0,17):
+    print((i+1), ".", item[i], "|", descript[i], "|", opt[i], "| $", cost[i], )
+iorb = input("\nWould you like to buy any items? b for buying items and d for looking in detail ")
+print(iorb)
+if iorb == "d":
+    while True:
+        try:
+            x = int(input("What item would you like to see in detail? "))
+            y = x-1
+            if x == 0:
+                False
+            if y <=-1:
+                print("please input an integer from 1 to 17")
+                continue
+
+            print(item[x], "|", descript[x], "|", opt[x], "|", cost[x], )
+        except:
+            print("f please input an integer from 1 to 17")
+elif iorb == "b":
+    print("This is the system for buying items. "
+          "\nYou can pick any integer from 1 to 4 for the amount of each item. "
+          "\nSay buy or nothing in options to continue to the buying stage."
+          "\nSay order to see all of the items on your current order. "
+          "\nSay finish when you have added all items to your order."
+          "\nSay cancel if you want to cancel the whole order.")
+    while True:
+        try:
+            kys = 0
+            o = str(input("What option would you like to use? "))
+            if o == "cancel":
+                c = str(input("Are you sure you want cancel your whole order?\nThis action can not be undone.\nSay yes or no "))
+                if c == "yes":
+                    print("cancelling order")
+                    receipt = []
+                    cost_stuff = []
+
+                elif c == "no":
+                    continue
+                else:
+                    print("please say yes or no")
+            elif o == "order":
+                print("printing current order")
+                for i in receipt:
+                    print(i)
+                    continue
+            elif o == "finish":
+                c = str(input("Are you sure you want to finish your order? Say yes or no "))
+                if c == "yes":
+                    print("finishing order")
+                    break
+                elif c == "no":
+                    continue
+                else:
+                    print("please say yes or no")
+            elif o == "" or o == "buy":
+                print("buying stage")
+                x = int(input("What item would you like to buy? "))
+                kys=1
+            else:
+                print("please choose a valid option")
+                continue
+            y = x - 1
+            if y <= -1:
+                print("please input an integer from 1 to 17")
+                continue
+            if y >= 17:
+                print("please input an integer from 1 to 17")
+                continue
+            if kys == 1:
+                if optyn[y] == "Y":
+                    print("options -", opt[y])
+                    copt = int(input("what option would you like? (choose an integer) "))
+                    if x == 1 or x == 7:
+                        print(copt, "\n", opt[y][(copt-1)], "\nprice - $", cost[y][copt-1])
+                    else:
+                        print(copt, "\n", opt[y][(copt-1)], "\nprice - $", cost[y])
+                else:
+                    kys = 1
+
+
+            if kys == 1:
+                a = int(input("How many of that item would you like? "))
+                b = a - 1
+                if b <= -1:
+                    print("please input an integer from 1 to 4")
+                    continue
+                if b >= 10:
+                    print("please input an integer from 1 to 4")
+                    continue
+            else:
+                continue
+            b = a - 1
+            if b <= -1:
+                print("please input an integer from 1 to 4")
+                continue
+            if b >= 10:
+                print("please input an integer from 1 to 4")
+                continue
+            print(item[x],"x",(a),"\nadded to order")
+            if x == 1 or x == 7:
+                f = float(cost[y][copt-1])*a
+            else:
+                f = (float(cost[y]))*a
+            print("cost for that amount of the item $", f)
+            if y == 1 or y == 7:
+                p = item[x],'x',(a),opt[y][copt],  cost[y]
+            else:
+                p = item[x],'x',(a), cost[y]
+            cost_stuff.append(f)
+            receipt.append(p)
+            hjk=1
+
+        except:
+            print("f please input an integer from 1 to 17 or a valid option")
+else: print("cringe")
+
+if hjk==1:
+    print("printing receipt")
+    for i in receipt:
+        print(i)
+    q = 0
+    for i in cost_stuff:
+        q = q + i
+    print("The final cost of everything is $",q)
+    root.mainloop()
+'''for i in range(ui('int', 'what item? ')):
+    list2.append(ui('int', "how many items?"))
+    continue'''
+while True:
+        '''try:
+            a = int(input("What item would you like to buy more of? "))
+            b = a-1
+            if x <=-1:
+                print("please input an integer from 1 to 17 for the item")
+                continue
+
+            print("deez")
+            print("buying", (iorb), "of item", (y))
+        except:
+            print("please input an integer from 1 to 17")'''
+
+'''
+        try:
+            x = int(input("enter your row"))
+            y = int(input("enter your column"))
+            if x <= 3 and y <= 3:
+                break
+                a = False
+                print("computer is not happy with you, try picking a integer from 1 to 3")
+            row = int(x - 1)
+            column = int(y - 1)
+            while board[row][column] != " ":
+                break
+                print("that space is already taken")
+        except:
+            print("computer is not happy with you, try picking a integer from 1 to 3")
+
+'''
+'''try:
+  print(x)
+except:
+  print("An exception occurred")'''
